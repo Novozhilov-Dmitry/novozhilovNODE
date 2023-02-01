604e14c00cd81ed1c24203bc8cc80e9730bcc77c
Nephrol <novozhilov2003@gmail.com>
2023-02-01 11:22:38 +0300

    SET

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+console.log("Filename:np", path.basename(__filename));
+console.log("Dirname:", path.dirname(__filename));
+console.log("Extension:", path.extname(__filename));
+console.log("Path object:", path.parse(__filename).dir);
 app.get("/", function (req, res) {
   res.send("GOT 'EM!");
 });
